5c2f42081044898d3f2940475f7eb51b50eaea5a
Raychel-310 <wakimoto.reina.infratop@gmail.com>
2023-09-19 17:17:14 +0000

    branch 1

---
+++ config/routes.rb
@@ -0,0 +1,27 @@
+Rails.application.routes.draw do
+  # 顧客用
+  # URL /customers/sign_in ...
+  devise_for :customers, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
+  # 管理者用
+  # URL /admin/sign_in ...
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+  
+  scope module: :public do
+    root to: 'homes#top'
+    get "customers" => "customers#show"
+    get "customers/edit" => "customers#edit"
+    patch "customers" => "customers#update"
+  end
+  
+  namespace :admin do
+    root to: 'homes#top'
+  end
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
