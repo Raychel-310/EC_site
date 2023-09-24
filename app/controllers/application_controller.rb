a8a0181c9f5a13d7e6f0eb40b077965139f7c91e
Raychel-310 <128213529+Raychel-310@users.noreply.github.com>
2023-09-25 01:59:49 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
 
-  def after_sign_in_path_for(resource)
-    customers_path
-  end
+  # def after_sign_in_path_for(resource)
+  #   customers_path
+  # end
 
   def after_sign_out_path_for(customer)
     root_path
