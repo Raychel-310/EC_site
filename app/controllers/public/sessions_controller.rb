c31584d68d04e91f91406bd3a3ac94edb0f5d79c
Raychel-310 <128213529+Raychel-310@users.noreply.github.com>
2023-09-25 01:59:22 +0900

    Update sessions_controller.rb

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,9 +2,9 @@
 
 class Public::SessionsController < Devise::SessionsController
   #before_action :configure_sign_in_params, only: [:create]
-  def after_sign_in_path_for(resource)
-    customers_path
-  end
+  # def after_sign_in_path_for(resource)
+  #   customers_path
+  # end
   
   def after_sign_out_path_for(customer)
     root_path
