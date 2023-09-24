58adce3a25b86f178cdd89e27b2f2871e8b43b63
Raychel-310 <128213529+Raychel-310@users.noreply.github.com>
2023-09-25 02:08:17 +0900

    Update registrations_controller.rb

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -52,7 +52,7 @@ class Public::RegistrationsController < Devise::RegistrationsController
 
   # The path used after sign up.
   def after_sign_up_path_for(resource)
-    customer_mypage_path
+    customers_mypage_path
   end
 
   # The path used after sign up for inactive accounts.
