672eb069ca4924c75cbb9ac0c08899f4bcafd113
Raychel-310 <wakimoto.reina.infratop@gmail.com>
2023-09-30 06:38:19 +0000

    branch3-2

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :genre
+  enum is_sale: {"販売中":true, "販売停止中":false}
   with_options presence: true do
    validates :name, presence: true
    validates :explanation, presence: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -20,6 +20,36 @@ Genre.create!(
  ]
 )
 
+Item.create!(
+ [
+  {
+    genre_id: 1,
+    item_name: "チーズケーキ",
+    explanation: "チーズケーキはチーズケーキでも、ニューヨークチーズケーキです。。",
+    unit_price_without_tax: 1000,
+    sale_status: 0,
+    image: open("./app/assets/images/cheesecake.jpg")
+  },
+  {
+    genre_id: 1,
+    item_name: "チョコレートケーキ",
+    explanation: "ただのチョコレートケーキです。",
+    unit_price_without_tax: 800,
+    sale_status: 0,
+    image: open("./app/assets/images/chocolatecake.jpg")
+  },
+  {
+    genre_id: 2,
+    item_name: "クッキー",
+    explanation: "クマにもらったクッキーです。",
+    unit_price_without_tax: 1000,
+    sale_status: 0,
+    image: open("./app/assets/images/cookie.jpg")
+  }
+  ]
+  )
+
+
 
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
