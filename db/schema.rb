76eff4959ecd2377b5afccd65ee4c52384762123
Raychel-310 <wakimoto.reina.infratop@gmail.com>
2023-10-01 13:47:59 +0000

    branch4

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_10_01_085007) do
+ActiveRecord::Schema.define(version: 2023_10_01_124235) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
